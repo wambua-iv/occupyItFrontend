6e6c418afcd58e7046f578ff440a112fb05c6167
Musalu Wambua <musaluwambua@gmail.com>
2022-07-08 15:36:40 +0300

    connected global state to app

--- utils/GlobalState.tsx
+++ utils/GlobalState.tsx
@@ -1,20 +1,48 @@
-import React, { ReactNode, createContext, useState } from 'react';
+import React, { ReactNode, createContext, useState, useEffect } from 'react';
+import _ from 'lodash';
 
 export const AuthContext = createContext<any>([]);
 export function Authprovider({ children }: { children: ReactNode }) {
   const [authState, setAuthState] = useState<any>({
     logged: false,
-    _id: '',
     tokens: {
       access_token: '',
       refresh_token: '',
     },
-    name: {
-      firstname: '',
-      lastname: '',
+    user: {
+      email: '',
+      name: {
+        firstname: '',
+        lastname: '',
+      },
+      ID: 0,
+      _id: '',
     },
-    ID: 0,
   });
+
+  useEffect(() => {
+    const state = sessionStorage.getItem('state');
+    console.log('my,y', state);
+    if (state && !_.isEqual(authState, JSON.parse(state))) {
+      let newState = JSON.parse(state);
+      setAuthState(() => ({
+        logged: newState?.logged,
+        tokens: {
+          access_token: newState.tokens.access_token,
+          refresh_token: newState?.tokens.refresh_token,
+        },
+        user: {
+          email: newState?.user.email,
+          ID: newState?.user.ID,
+          name: {
+            firstname: newState?.user.name.firstname,
+            lastname: newState?.user.name.lastname,
+          },
+          _id: newState?.user._id,
+        },
+      }));
+    }
+  }, [authState, setAuthState]);
 
   return (
     <AuthContext.Provider value={[authState, setAuthState]}>
