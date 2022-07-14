3ff9e0bb5719f74cb4c47b556c3d4ad42665c1a6
Musalu Wambua <musaluwambua@gmail.com>
2022-07-14 22:28:47 +0300

    listings from database and type support

--- src/components/property-listings/Listings.tsx
+++ src/components/property-listings/Listings.tsx
@@ -1,65 +1,17 @@
 import { Box, Container, Grid, Paper, Typography } from '@mui/material';
 import Link from 'next/link';
 import React from 'react';
+import { ListingType } from '.';
 
-interface ListingType {
-  id: number;
-  image: any;
-  title: string;
-  description: string;
-  location: string;
-  amenities?: string[];
-  price: number;
-}
-
-function Listings() {
-  const listings = [
-    {
-      id: 1,
-      image: '/home2.jpg',
-      title: 'Executive Apartments',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cum eos, veritatis animi eligendi impedit voluptas obcaecati culpa eveniet amet, laboriosam, assumenda vero ipsa doloribus modi quidem. Voluptatibus, itaque unde.',
-      location: 'Juja',
-      amenities: ['garage', 'wifi'],
-      price: 25000,
-    },
-    {
-      id: 2,
-      image: '/home2.jpg',
-      title: 'Executive Apartments 2',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cum eos, veritatis animi eligendi impedit voluptas obcaecati culpa eveniet amet, laboriosam, assumenda vero ipsa doloribus modi quidem. Voluptatibus, itaque unde.',
-      location: 'Juja',
-      price: 25000,
-    },
-    {
-      id: 3,
-      image: '/home2.jpg',
-      title: 'Executive Apartments 3',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cum eos, veritatis animi eligendi impedit voluptas obcaecati culpa eveniet amet, laboriosam, assumenda vero ipsa doloribus modi quidem. Voluptatibus, itaque unde.',
-      location: 'Juja',
-      price: 25000,
-    },
-    {
-      id: 4,
-      image: '/home2.jpg',
-      title: 'Executive Apartments 4',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cum eos, veritatis animi eligendi impedit voluptas obcaecati culpa eveniet amet, laboriosam, assumenda vero ipsa doloribus modi quidem. Voluptatibus, itaque unde.',
-      location: 'Juja',
-      price: 25000,
-    },
-  ];
+function Listings({ listings }: any) {
   return (
     <Container maxWidth="lg">
       <Box sx={{ p: 2, backgroundColor: '#e5e5e5', borderRadius: '.5rem' }}>
         <Typography variant="h5">Rental Properties</Typography>
         <Box sx={{ my: 2 }}>
           <Grid container spacing={2}>
-            {listings.map((listing) => (
-              <Grid item key={listing.id} xs={12} sm={6} md={3}>
+            {listings.map((listing: ListingType) => (
+              <Grid item key={listing._id} xs={12} sm={6} md={3}>
                 <LIstingCards {...listing} />
               </Grid>
             ))}
@@ -70,7 +22,14 @@ function Listings() {
   );
 }
 
-function LIstingCards({ image, title, price, location, amenities }: ListingType) {
+function LIstingCards({
+  _id,
+  images,
+  property_name,
+  price,
+  location,
+  amenities,
+}: ListingType) {
   return (
     <Paper
       sx={{
@@ -80,8 +39,8 @@ function LIstingCards({ image, title, price, location, amenities }: ListingType)
     >
       <Box
         component="img"
-        src={image}
-        alt={title}
+        src={images[0]}
+        alt={property_name}
         sx={{
           margin: 'auto',
           width: '100%',
@@ -90,22 +49,48 @@ function LIstingCards({ image, title, price, location, amenities }: ListingType)
           objectFit: 'cover',
         }}
       />
-      <Link href={title}>
-        <Typography sx={{ fontWeight: 600, px: 1, mt: 1, cursor: 'pointer' }}>{title}</Typography>
+      <Link
+        href={{
+          pathname: '/[_id]',
+          query: { _id:  _id  },
+        }}
+      >
+        <Typography sx={{ fontWeight: 600, px: 1, mt: 1, cursor: 'pointer' }}>
+          {property_name}
+        </Typography>
       </Link>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2, px: 1 }}>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'space-between', mt: 2, px: 1 }}
+      >
         <Typography variant="subtitle2">Price</Typography>
-        <Typography sx={{ fontSize: '.95rem', fontWeight: 600 }}>Ksh. {price}</Typography>
+        <Typography sx={{ fontSize: '.95rem', fontWeight: 600 }}>
+          Ksh. {price}
+        </Typography>
       </Box>
 
-      {amenities ? <Typography sx={{ px: 2, fontSize: '.85rem', fontWeight: 600 }}>Amenities</Typography> : <div></div>}
-      {amenities?.map((amenity) => (
-        <Typography key={amenity} sx={{ px: 2, fontSize: '.75rem', textAlign: 'right' }}>
-          {amenity}
+      {amenities ? (
+        <Typography sx={{ px: 2, fontSize: '.85rem', fontWeight: 600 }}>
+          Amenities
         </Typography>
-      ))}
+      ) : (
+        <div></div>
+      )}
+      {amenities ? (
+        <>
+          <Typography sx={{ px: 2, fontSize: '.75rem', textAlign: 'right' }}>
+            {amenities.bedrooms}
+          </Typography>
+          <Typography sx={{ px: 2, fontSize: '.75rem', textAlign: 'right' }}>
+            {amenities.washroom} washroom
+          </Typography>
+        </>
+      ) : (
+        <></>
+      )}
 
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 2, px: 1 }}>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'space-between', my: 2, px: 1 }}
+      >
         <Typography variant="subtitle2">Location</Typography>
         <Typography>{location}</Typography>
       </Box>
