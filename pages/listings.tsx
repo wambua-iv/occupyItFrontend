a4eba79113455b9b821de4c5419a84ca05fe3520
Musalu Wambua <musaluwambua@gmail.com>
2022-07-14 12:50:11 +0300

    listings database call

--- pages/listings.tsx
+++ pages/listings.tsx
@@ -1,11 +1,23 @@
+import { NextPageContext } from 'next';
 import React from 'react';
 import PropertyListings from '../src/components/property-listings';
 
-function PropertyList() {
+function PropertyList({ props }: any) {
+ const [listings, setListings] =React.useState<any>([])
+ React.useEffect(() =>{
+  (async() => {
+    await fetch('http://127.0.0.1:3090/properties/listings')
+    .then((res) => res.json())
+    .then((data) => setListings(data))
+    .catch()
+  })()
+ }, [setListings])
+
   return (
     <div style={{ width: '100%', margin: 0 }}>
-      <PropertyListings />
+      <PropertyListings listings={listings}/>
     </div>
   );
 }
+
 export default PropertyList;
