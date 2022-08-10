22a2c5f830fdd75d82fea202da31e35c529bde1a
Wambua Musalu <musaluwambua@gmail.com>
2022-08-10 14:10:17 +0300

    partial deployment

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,13 +1,13 @@
 import Router from 'next/router';
 import React from 'react';
 import { Loading } from '../src/components/utils';
-import { AuthContext } from '../utils/GlobalState';
+import { AuthContext, Authenticated } from '../utils/GlobalState';
 
 function Logout() {
   const[AuthState,setAuthState] = React.useContext(AuthContext)
   React.useEffect(() => {
     sessionStorage.clear();
-    setAuthState? setAuthState((AuthState) => ({ ...AuthState, 'logged': false})) : Router.push('/');
+    setAuthState? setAuthState((AuthState: Authenticated) => ({ ...AuthState, 'logged': false})) : Router.push('/');
     Router.push('/')
   }, [setAuthState, AuthState]);
   return <Loading redirectUrl="/" />;
