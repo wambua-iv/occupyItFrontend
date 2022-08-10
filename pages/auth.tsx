99756fc1307de32f30f183c6fab71635efa01977
Wambua Musalu <musaluwambua@gmail.com>
2022-08-10 13:38:04 +0300

    Authetication page refactor

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Auth from '../src/components/authentication';
+import { AuthContext } from '../utils/GlobalState';
 
 function Authentication() {
   return (
     <div style={{ width: '100%', height: '100%', margin: 0 }}>
-      <Auth />
+      <AuthContext.Consumer>
+        {([authState, setAuthState]) => (
+          <Auth authState={authState} setAuthState={setAuthState} />
+        )}
+      </AuthContext.Consumer>
     </div>
   );
 }
